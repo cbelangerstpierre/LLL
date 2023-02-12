426979fcde8d81426626a35b7382ad8ee6c2aa25
cbelangerstpierre <cedricbstpierre@yahoo.com>
2023-02-12 13:31:05 -0500

    Added other math files directly into this repo for easier search Added a lot of comments in each file describing the code and the maths behind it

--- lll.py
+++ lll.py
@@ -1,49 +1,71 @@
+# The LLL function takes in a list of vectors and a value for the reduction parameter delta
 def LLL(vectors, delta):
+    # A function to calculate the inner product of two vectors
     def innerProduct(vector1, vector2):
         return sum([vector1[pos] * vector2[pos] for pos in range(len(vector1))])
-
+    
+    # A function to calculate the difference between two vectors
     def difference(vector1, vector2):
         return [vector1[pos] - vector2[pos] for pos in range(len(vector1))]
-
+    
+    # A function to calculate the summation of vectors
     def summations(vectors_):
         return [sum([vector[pos] for vector in vectors_]) for pos in range(len(vectors_[0]))]
-
+    
+    # A function to multiply a vector by a scalar
     def multiplication(vector, amount):
         return [vector[pos] * amount for pos in range(len(vector))]
-
+    
+    # A function to determine if two vectors meet the Lovasz condition
     def lovasz(reduced1, reduced2, base2):
         return innerProduct(reduced2, reduced2) >= (
                 delta - (innerProduct(base2, reduced1) / innerProduct(reduced1, reduced1)) ** 2) * innerProduct(
             reduced1, reduced1)
-
+    
+    # A function to perform the Gram-Schmidt orthogonalization
     def gramSchimdt(B, Bstar, pos):
         vectorsTemp = []
+        # Calculate the projections of the new vector onto all previous orthogonal vectors
         for incrementer in range(pos):
             vectorsTemp.append(multiplication(Bstar[incrementer],
                                               innerProduct(B[pos], Bstar[incrementer]) / innerProduct(
                                                   Bstar[incrementer], Bstar[incrementer])))
-
+        # Calculate the orthogonal vector
         finalStar = difference(B[pos], summations(vectorsTemp))
         final = B[pos]
+        # Project the new vector onto all previous orthogonal vectors
         for incrementer in range(pos):
             final = difference(final, multiplication(B[incrementer], round(
                 innerProduct(final, Bstar[incrementer]) / innerProduct(Bstar[incrementer], Bstar[incrementer]))))
         return final, finalStar
-
+    
+    # Initialize the iteration counter
     i = 1
-
+    
+    # Repeat the loop until all vectors have been processed
     while i < len(vectors):
+        # Copy the original vectors
         reduced_vectors = vectors.copy()
+        # Iterate through all previous vectors
         for j in range(1, i + 1):
+            # Perform the Gram-Schmidt orthogonalization on the current vector
             vectors[i], reduced_vectors[i] = gramSchimdt(vectors, reduced_vectors, i)
 
+            # Check if the length of the new vector is too large and needs to be reduced
             if not lovasz(reduced_vectors[i - 1], reduced_vectors[i], vectors[i]):
+                # Swap the current and previous vectors
                 vectors[i - 1], vectors[i] = vectors[i], vectors[i - 1]
+                
+                # Decrement the counter so that the previous vector can be processed again
                 i -= 1
+                
+        # Increment the counter to move to the next vector
         i += 1
-
+        
+    # Return the processed vectors
     return vectors
 
 
+# Test the LLL function with two sets of vectors
 print(LLL([[201, 37], [1648, 297]], 0.75))
 print(LLL([[15, 23, 11], [46, 15, 3], [32, 1, 1]], 0.75))
---
+++ primitive_roots.py
@@ -0,0 +1,64 @@
+import math
+import timeit
+import numpy
+import sympy
+
+# This function is used to perform a number of tests to determine if a number is a primitive root modulo `mod`.
+def onlyTest(mod):
+    # Calculate `mod - 1` and store the result in `mod_minus1` to not repeatedly make the operation
+    mod_minus1 = mod - 1
+    # Use `sympy.primefactors` to get the prime factors of `mod_minus1`, then map the function `mod_minus1 // x` (Integer division) over the list of prime factors to get our tests.
+    tests = list(map(lambda x: mod_minus1 // x, sympy.primefactors(mod_minus1)))
+    # Return a list of all numbers `a` between 1 and `mod - 1` inclusive, where `a` is not a perfect square and does not equal `1` when raised to the power of each test in `tests` modulo `mod`.
+    return [a for a in range(1, mod) if math.isqrt(a) ** 2 != a and all(pow(a, test, mod) != 1 for test in tests)]
+
+# This function performs the same tests as `onlyTest` in order to find the first primitive root and generates a list of `m` values for each other primitive roots.
+def testAndMGenerate(mod):
+    # Calculate `mod - 1` and store the result in `mod_minus1` to not repeatedly make the operation
+    mod_minus1 = mod - 1
+    # Use `sympy.primefactors` to get the prime factors of `mod_minus1`, then map the function `mod_minus1 // x` (Integer division) over the list of prime factors to get our tests.
+    tests = list(map(lambda x: mod_minus1 // x, sympy.primefactors(mod_minus1)))
+    # Loop over all numbers `a` between 1 and `mod - 1` inclusive.
+    for a in range(1, mod):
+        # If `a` is not a perfect square and does not equal `1` when raised to the power of each test in `tests` modulo `mod`, return a sorted list of `m` values, where each `m` is calculated as `pow(a, m, mod)`, with `m` ranging from 1 to `mod - 1` inclusive and `math.gcd(m, mod_minus1) == 1` to get every primitive roots.
+        if math.isqrt(a) ** 2 != a and all(pow(a, test, mod) != 1 for test in tests):
+            return sorted([pow(a, m, mod) for m in range(1, mod) if math.gcd(m, mod_minus1) == 1])
+
+
+def primesfrom2to(n):
+    # Create a NumPy array of ones with a length of n // 3 + (n % 6 == 2)
+    # The data type of the array is set to boolean
+    sieve = numpy.ones(n // 3 + (n % 6 == 2), dtype=bool)
+    
+    # Loop through the range from 1 to int(n ** 0.5) // 3 + 1
+    for i in range(1, int(n ** 0.5) // 3 + 1):
+        # If the value at index i of the sieve array is True
+        if sieve[i]:
+            # Calculate k as 3 * i + 1 with a bitwise OR with 1
+            k = 3 * i + 1 | 1
+            # Set every 2k-th value in the sieve array starting from k * k // 3 to False
+            sieve[k * k // 3::2 * k] = False
+            # Set every 2k-th value in the sieve array starting from k * (k - 2 * (i & 1) + 4) // 3 to False
+            sieve[k * (k - 2 * (i & 1) + 4) // 3::2 * k] = False
+    
+    # Return a concatenated array of 2, 3, and the result of adding 1 to every value in the sieve array
+    # that is the result of 3 * the non-zero values in the sieve array starting from the second value
+    return numpy.r_[2, 3, ((3 * numpy.nonzero(sieve)[0][1:] + 1) | 1)]
+
+def test_function(function):
+    # Record the start time
+    start = timeit.default_timer()
+    
+    # Loop through the list of prime numbers up to a certain number
+    for q in list(map(int, primesfrom2to(20000))):
+        # Call the function argument and pass the prime number as an argument
+        result = function(q)
+        # print(q, "->", result) # Code to add to see the results
+    
+    # Print the time it took to run the function on all prime numbers
+    print("Time :", timeit.default_timer() - start)
+
+# Call the test_function with onlyTest as the argument
+test_function(onlyTest)
+# Call the test_function with testAndMGenerate as the argument
+test_function(testAndMGenerate)
