99e405a72940b9e72fa27b958db8a40be25c54e1
cbelangerstpierre <81055839+cbelangerstpierre@users.noreply.github.com>
2023-03-22 16:55:12 -0400

    Update idk.py

--- idk.py
+++ idk.py
@@ -6,17 +6,21 @@ from utils import get_longest_length, get_pairs_count, is_basis
 from plot import get_plot
 from lll import LLL
 from primitive_roots import get_first_primitive
-from vector_generator import get_vector_generator2
+from vector_generator import get_vector_generator2, get_vector_generator3
 from vector_generator import get_vector_generator
 
 
-Q = 24611
+Q = 2000003
 
 start = timeit.default_timer()
 gen = get_first_primitive(Q)
 print("Time : " + str(timeit.default_timer() - start))
+# start = timeit.default_timer()
+# V = get_vector_generator2(Q, gen)
+# print("Time : " + str(timeit.default_timer() - start))
+# print(V)
 start = timeit.default_timer()
-V = get_vector_generator2(Q, gen)
+V = get_vector_generator3(Q, gen)
 print("Time : " + str(timeit.default_timer() - start))
 print(V)
 start = timeit.default_timer()
